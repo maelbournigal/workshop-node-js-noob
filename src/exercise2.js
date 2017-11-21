3fdac341ac1e127d5438d0d058f2a2079a65592d
mael <mael.bournigal@ynov.com>
2017-11-21 16:42:27 +0100

    modif exercice2

--- src/exercise2.js
+++ src/exercise2.js
@@ -1,6 +1,6 @@
 exports.run = function(input) {
   var separation = input.split(" , ");
-  for( var i = 0; i<separation.lenght; i++){
+  for( var i = 0; i < separation.lenght; i++){
     var test = 0;
     if(separation[i]>test){test = separation[i]};
   }
