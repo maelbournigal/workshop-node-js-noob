37b79a04de414561ee5fe974bccbca40d2891ab3
mael <mael.bournigal@ynov.com>
2017-11-21 17:10:23 +0100

    modification des 3 premiers exercices

--- src/exercise2.js
+++ src/exercise2.js
@@ -1,8 +1,10 @@
 exports.run = function(input) {
-  var separation = input.split(" , ");
-  for( var i = 0; i < separation.lenght; i++){
-    var test = 0;
-    if(separation[i]>test){test = separation[i]};
+  var separation = input.split("");
+  var test = 0;
+  for( var i = 0; i < separation.length; i++){
+    if(parseInt(separation[i])>test){
+      console.log(parseInt(separation[i])) 
+      test = parseInt(separation[i])};
   }
   console.log(test);
 };
--- src/exercise3.js
+++ src/exercise3.js
@@ -1,6 +1,6 @@
 exports.run = function(input) {
   var result = 1;
-  for(var i = 0; i<input.lenght; i++){
+  for(var i = 0; i<input.length; i++){
     if(input[i].multiply === true){result = result * input[i].value}
   }
   console.log(result);
