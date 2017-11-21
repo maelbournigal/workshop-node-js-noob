5bc3b859275c8bb1f47363096bb53bbdbe4db178
mael <mael.bournigal@ynov.com>
2017-11-21 16:37:59 +0100

    exercice2

--- src/exercise2.js
+++ src/exercise2.js
@@ -1,4 +1,8 @@
 exports.run = function(input) {
-  var separation = input.split( );
-  console.log();
+  var separation = input.split(" , ");
+  for( var i = 0; i<separation.lenght; i++){
+    var test = 0;
+    if(separation[i]>test){test = separation[i]};
+  }
+  console.log(test);
 };
